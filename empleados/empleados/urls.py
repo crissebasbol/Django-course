5ac7c5d0d6369e5de3ec676a5d143bbc08c8e1b1
crissebasbol <crissebasbol@gmail.com>
2021-05-23 21:17:37 -0500

    Checking urls

--- empleados/empleados/urls.py
+++ empleados/empleados/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+def PruebaUrl(self):
+    print("=======TESTING URLS======")
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('test/', PruebaUrl)
 ]
