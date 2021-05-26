96870795ce9f4bba699f163bd2a0d85c4d243bb8
crissebasbol <crissebasbol@gmail.com>
2021-05-25 20:12:28 -0500

    create models and make migrations

--- empleados/applications/home/models.py
+++ empleados/applications/home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Prueba(models.Model):
+    title = models.CharField(max_length=100)
+    subtitle = models.CharField(max_length=50)
+    cantidad = models.IntegerField()
+
+    def __str__(self):
+        return self.title
