333037dd63e07ba5e5561a08faff01584684b5e8
crissebasbol <crissebasbol@gmail.com>
2021-05-25 22:30:43 -0500

    Exploring some parameters db

--- empleados/applications/departamento/models.py
+++ empleados/applications/departamento/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Departamento(models.Model):
+    name = models.CharField('Name', max_length=50, blank=True, null=True)
+    short_name = models.CharField('Short name', max_length=20, unique=True)
+    anulate = models.BooleanField('Anulate', default=False)
+
+    def __str__(self):
+        return str(self.id) + ', ' + self.name + ', '+ self.short_name
