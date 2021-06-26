96b1681b050509f4cae2e4e18cf6691e579cd72a
crissebasbol <crissebasbol@gmail.com>
2021-06-25 21:01:55 -0500

    Create list personas

---
+++ agendadj/agenda/applications/persona/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, re_path
+
+from . import views
+
+app_name = "persona_app"
+
+urlpatterns = [
+    path(
+        "personas/",
+        views.ListPersonaListView.as_view(),
+        name="personas"
+    )
+]
--- agendadj/agenda/applications/persona/views.py
+++ agendadj/agenda/applications/persona/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
+from .models import Person
 
+class ListPersonaListView(ListView):
+    template_name = "persona/personas.html"
+    context_object_name = "personas"
 
+    def get_queryset(self):
+        return Person.objects.all()
 
